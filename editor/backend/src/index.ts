8de63077e2ab64f6b191e813b998db80d731ed61
Space_Fox <44732812+emanuelfranklyn@users.noreply.github.com>
2024-12-07 00:43:18 -0300

    (feat): running code from browser editor working (beta)
    Co-authored-by: João Gabriel Vieira Silva <Osoapy@users.noreply.github.com>

--- editor/backend/src/index.ts
+++ editor/backend/src/index.ts
@@ -1,49 +1,162 @@
-const { exec } = require('child_process');
-const path =require('path');
-const fs = require('fs');
-const { Socket } = require('net');
+import { ConsoleEngine, Engine, Logger,  } from '@promisepending/logger.js';
+import { randomUUID } from 'crypto';
+import { spawn } from 'child_process';
+import fs from 'fs';
+import ws from 'ws';
+import path from 'path';
 
-const command = 'prin';
+const serverPort = 2018;
 
-if (!fs.existsSync(path.resolve(__dirname, `../cache`))) fs.mkdirSync(path.resolve(__dirname, `../cache`));
-const randomId = Math.random().toString(36).substring(7);
-const cachePath = path.resolve(__dirname, `../cache/${randomId}.lua`);
-fs.writeFileSync(cachePath, command);
+interface IMessageStruct {
+  name: string,
+  data: any
+}
 
-exec(path.resolve(__dirname, `../languageServer/bin/lua-language-server --socket=3520`), (error: any, stdout: any, stderr: any) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
+interface chatMessage {}
+interface UserMessage extends chatMessage {}
+interface BotMessage extends chatMessage {}
+interface BotSettings {}
+
+class LoggableClass {
+  public loggerEngine: Engine;
+  constructor(engine: Engine) {
+    this.loggerEngine = engine;
   }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
+}
 
-  // try to send auto completion language server request to the server
-  const client = new Socket();
-  client.connect(5050, 'localhost', () => {
-    console.log('Connected');
-    client.on('data', (data: any) => {
-      console.log('Received: ' + data);
+class FakeDiscord extends LoggableClass {
+  private logger: Logger;
+  private uuid: string;
+  private socket: ws;
+  private userName: string;
+  private icon: number;
+  private chat: (UserMessage | BotMessage)[];
+  private botSettings: BotSettings;
+  private runnerProcess: any;
+  private isRunning: boolean;
+
+  constructor(engine: Engine, socket: ws) {
+    super(engine);
+    this.socket = socket;
+    this.uuid = randomUUID();
+    // set icon as a random number between 0 and 5
+    this.icon = Math.floor(Math.random() * 6);
+    this.logger = new Logger({ prefixes: [`User-${this.uuid}`] });
+    this.loggerEngine.registerLogger(this.logger);
+
+    this.logger.debug(`Hello! i'm ${this.uuid}!`);
+
+    this.sendEvent('RegisterUUID', this.uuid);
+    this.socket.on('message', this.messageHandler.bind(this));
+    this.socket.on('close', this.onDisconnect.bind(this));
+  }
+
+  getUUID(): string {
+    return this.uuid;
+  }
+
+  sendEvent(eventName: string, data: any) {
+    this.socket.send(JSON.stringify({ name: eventName, data }));
+  }
+
+  messageHandler(message: string) {
+    let msg: IMessageStruct | null = null;
+    try {
+      msg = JSON.parse(message);
+    } catch {
+      this.logger.error('Invalid message received from client!');
+    }
+    if (!msg) return;
+
+    if (msg.name === 'setName') this.setName(msg.data.toString());
+    else if (msg.name === 'executeCode') this.runInterpreter(msg.data.toString());
+    else if (msg.name === 'stopCodeExecution') this.stopCodeExecution();
+
+    this.sendStatus();
+  }
+
+  setName(name: string) {
+    this.userName = name;
+  }
+
+  sendStatus() {
+    this.logger.debug('Sending user status...');
+    this.sendEvent('userStatus', {
+      uuid: this.uuid,
+      name: this.userName,
+      icon: this.icon,
+    })
+  }
+
+  runInterpreter(code: string) {
+    if (this.isRunning) this.stopCodeExecution();
+    // write the code to a file
+    const userCodeFile = path.resolve(__dirname, '..', 'cache', this.uuid + '.lua');
+    if (!fs.existsSync(path.resolve(__dirname, '..', 'cache'))) fs.mkdirSync(path.resolve(__dirname, '..', 'cache'));
+    fs.writeFileSync(userCodeFile, code);
+
+    this.sendEvent('startingCodeExecution', null);
+
+    // creates a sub-process to run the interpreter
+    this.runnerProcess = spawn(path.resolve(__dirname, '..', 'interpreter', 'luvit'), [userCodeFile], { stdio: 'pipe' });
+    this.runnerProcess.stdout.on('data', (data: any) => {
+      this.sendEvent('interpreterOutput', data.toString());
     });
 
-    client.write(JSON.stringify({
-      jsonrpc: '2.0',
-      id: 1,
-      method: 'textDocument/completion',
-      params: {
-        textDocument: {
-          uri: 'file://' + cachePath,
-        },
-        position: {
-          line: 0,
-          character: 4,
-        },
-      },
-    }));
+    this.runnerProcess.stderr.on('data', (data: any) => {
+      this.sendEvent('interpreterOutputErr', data.toString());
+    });
 
-    client.end();
-  });
+    this.runnerProcess.on('close', (code: number) => {
+      this.sendEvent('interpreterClosed', code);
+    });
+  }
 
+  stopCodeExecution() {
+    if (this.runnerProcess) this.runnerProcess.kill();
+  }
 
-  // fs.unlinkSync(cachePath);
-});
+  onDisconnect() {
+    this.logger.info('Client disconnected!');
+    // delete their cache file
+    const userCodeFile = path.resolve(__dirname, '..', 'cache', this.uuid + '.lua');
+    if (this.runnerProcess) this.runnerProcess.kill();
+    if (fs.existsSync(userCodeFile)) fs.unlinkSync(userCodeFile);
+  }
+}
+
+class Main extends LoggableClass {
+  private logger: Logger;
+  private wsServer: ws.Server;
+  private Users: FakeDiscord[];
+
+  constructor(engine: Engine) {
+    super(engine);
+    this.logger = new Logger({
+      prefixes: ['MiniLua-Backend'],
+      coloredBackground: true,
+      allLineColored: true,
+      disableFatalCrash: true,
+    });
+    engine.registerLogger(this.logger);
+    this.Users = [];
+
+    this.logger.info('Starting MiniLua Backend...');
+  }
+
+  startWebServer() {
+
+  }
+
+  startTerminalServer() {
+    this.wsServer = new ws.Server({ port: serverPort });
+    this.wsServer.on('connection', (socket) => {
+      const user = new FakeDiscord(this.loggerEngine, socket);
+      this.Users.push(user);
+    })
+  }
+}
+
+const main = new Main(new ConsoleEngine({ debug: true }));
+main.startTerminalServer();
+main.startWebServer();
--- editor/index.js
+++ editor/index.js
@@ -1,9 +1,106 @@
-var editor = monaco.editor.create(document.getElementById("editor"), {
-  value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join("\n"),
-  language: "lua",
+const backendWebsocketPort = 2018;
+const backendURLWebsocket = window.location.hostname;
+
+/* Validar input */
+document.addEventListener("DOMContentLoaded", () => {
+  const inputField = document.getElementById("loginUsername");
+  const submitButton = document.getElementById("loginButton");
+
+  inputField.addEventListener("input", () => {
+    const alphabeticChars = inputField.value.replace(/[^a-zA-Z]/g, "").length;
+    submitButton.disabled = alphabeticChars < 5;
+  });
 });
 
-editor.getModel().updateOptions({ tabSize: 2, insertSpaces: true });
+// prevents user from tabbing into elements that should not be selectable
+window.addEventListener('focusin', (e) => {
+  if (!!document.activeElement.closest('.unselectable')) {
+    const item = document.body.querySelector(':not(.unselectable):not([style*="display:none"]):not([style*="display: none"])');
+    if (item) {
+      const firstFocusableElement = item.querySelector('input, select, textarea, button, object, a, area[href], [tabindex]')
+      if (firstFocusableElement) firstFocusableElement.focus();
+    }
+  }
+});
+
+function updateClientData(name, iconId) {
+  document.getElementById('clientUserImage').src = `./assets/profilePics/${iconId}.jpeg`;
+}
+
+class Client {
+  editor = null;
+  websocket = null;
+  uuid = null;
+  username = null;
+  icon = null;
+  constructor() {
+    this.editor = monaco.editor.create(document.getElementById("editor"), {
+      value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join("\n"),
+      language: "lua",
+    });
+    this.editor.getModel().updateOptions({ tabSize: 2, insertSpaces: true });
+
+    monaco.editor.setTheme("vs-dark");
+
+    window.addEventListener("resize", () => {
+      this.editor.layout();
+    });
+
+    ///////////////////
+
+    this.websocket = new WebSocket(`ws://${backendURLWebsocket}:${backendWebsocketPort}`);
+    this.websocket.addEventListener('message', this.messageHandler.bind(this));
+
+    document.getElementById('loginButton').addEventListener('click', () => {
+      const username = document.getElementById('loginUsername').value;
+      this.sendEvent('setName', username);
+    });
+
+    document.getElementById('runButton').addEventListener('click', () => {
+      this.callRunCode();
+    });
+  }
+
+  sendEvent(name, data) {
+    if (this.websocket.readyState !== 1) return;
+    this.websocket.send(JSON.stringify({ name, data }));
+  }
+
+  messageHandler(message) {
+    const msg = JSON.parse(message.data);
+    
+    console.log(msg);
+    if (msg.name === 'userStatus') {
+      document.getElementById('loginModal').style.display = 'none';
+      document.getElementById('pageContent').classList.remove('unselectable');
+      this.username = msg.data.username;
+      this.icon = msg.data.icon;
+      this.uuid = msg.data.uuid;
+      updateClientData(this.username, this.icon);
+    } else if (msg.name === 'startingCodeExecution') {
+
+    } else if (msg.name === 'interpreterOutput') {
+      this.showInTerminal(msg.data);
+    } else if (msg.name === 'interpreterOutputErr') {
+      this.showInTerminal(msg.data);
+    } else if (msg.name === 'interpreterClosed') {
+    }
+  }
+
+  showInTerminal(text) {
+    const terminal = document.getElementById('consoleContent');
+    const p = document.createElement('p');
+    p.textContent = text;
+    terminal.appendChild(p);
+  }
+
+  callRunCode() {
+    const code = this.editor.getValue();
+    this.sendEvent('executeCode', code);
+  }
+}
+
+const client = new Client();
 
 // monaco.editor.defineTheme("vs-dark", {
 //   base: "vs-dark",
@@ -13,16 +110,3 @@ editor.getModel().updateOptions({ tabSize: 2, insertSpaces: true });
 //     "editor.background": "#000000",
 //   },
 // });
-
-monaco.editor.setTheme("vs-dark");
-
-window.addEventListener("resize", () => {
-  editor.layout();
-});
-
-document.getElementById("runButton").addEventListener("click", () => {
-  var code = editor.getValue();
-  var script = document.createElement("script");
-  script.innerHTML = code;
-  document.body.appendChild(script);
-});
